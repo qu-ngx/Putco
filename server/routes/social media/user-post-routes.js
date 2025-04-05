f571e86d950c10c884e82eb13e6870cf7805ea7b
chingo09 <chingophuong36@gmail.com>
2025-04-05 04:19:50 -0400

    Create user-post-routes.js

---
+++ server/routes/social media/user-post-routes.js
@@ -0,0 +1,25 @@
+const express = require ("express");
+const { protectRoute } = require ("../middleware/protectRoute.js");
+const {
+	commentOnPost,
+	createPost,
+	deletePost,
+	getAllPosts,
+	getFollowingPosts,
+	getLikedPosts,
+	getUserPosts,
+	likeUnlikePost,
+} = require ("../controllers/social-media/user-post-controller.js");
+
+const router = express.Router();
+
+router.get("/all", protectRoute, getAllPosts);
+router.get("/following", protectRoute, getFollowingPosts);
+router.get("/likes/:id", protectRoute, getLikedPosts);
+router.get("/user/:userName", protectRoute, getUserPosts);
+router.post("/create", protectRoute, createPost);
+router.post("/like/:id", protectRoute, likeUnlikePost);
+router.post("/comment/:id", protectRoute, commentOnPost);
+router.delete("/:id", protectRoute, deletePost);
+
+module.exports = router;
